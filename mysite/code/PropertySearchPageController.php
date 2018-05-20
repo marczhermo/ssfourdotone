c1363ce4e1cab0499b2b72785f36638df3923790
Marco Hermo <marco@silverstripe.com>
2018-05-21 06:49:36 +1200

    Add Radio buttons for selecting search engines

--- mysite/code/PropertySearchPageController.php
+++ mysite/code/PropertySearchPageController.php
@@ -4,6 +4,7 @@ namespace Acme;
 
 use PageController;
 use SilverStripe\Forms\CheckboxSetField;
+use SilverStripe\Forms\OptionsetField;
 use SilverStripe\Forms\Form;
 use SilverStripe\Forms\FieldList;
 use SilverStripe\Forms\TextField;
@@ -20,7 +21,7 @@ class PropertySearchPageController extends PageController
 
     public function index(HTTPRequest $request)
     {
-        $properties = $this->createSearch($request->getVar('Keywords'), 'Properties', 'MySQL');
+        $properties = $this->createSearch($request->getVar('Keywords'), 'Properties', $request->getVar('Client'));
 
         if ($arrival = $request->getVar('ArrivalDate')) {
             $arrivalStamp = strtotime($arrival);
@@ -66,11 +67,11 @@ class PropertySearchPageController extends PageController
     public function PropertySearchForm()
     {
         $nights = [];
-        foreach(range(1,14) as $i) {
+        foreach (range(1, 14) as $i) {
             $nights[$i] = "$i night" . (($i > 1) ? 's' : '');
         }
         $prices = [];
-        foreach(range(0, 400, 50) as $i) {
+        foreach (range(0, 400, 50) as $i) {
             $prices[$i] = '$'.$i;
         }
 
@@ -79,16 +80,20 @@ class PropertySearchPageController extends PageController
             'PropertySearchForm',
             FieldList::create(
                 TextField::create('Keywords')->setAttribute('placeholder', 'Please type here...'),
-                TextField::create('ArrivalDate','Arrival date'),
+                TextField::create('ArrivalDate', 'Arrival date'),
                 DropdownField::create('Nights', 'No. of nights')->setEmptyString('-- any --')->setSource($nights),
                 DropdownField::create('Bedrooms', 'No. of bedrooms')->setEmptyString('-- any --')->setSource(ArrayLib::valuekey(range(1,5))),
                 DropdownField::create('Bathrooms', 'No. of bathrooms')->setEmptyString('-- any --')->setSource(ArrayLib::valuekey(range(1,5))),
                 DropdownField::create('MinPrice', 'Min. price')->setEmptyString('-- any --')->setSource($prices),
                 DropdownField::create('MaxPrice', 'Max. price')->setEmptyString('-- any --')->setSource($prices),
-                CheckboxSetField::create('RegionID', 'Regions', Region::get()->map('ID', 'Title'))
+                CheckboxSetField::create('RegionID', 'Regions', Region::get()->map('ID', 'Title')),
+                OptionsetField::create('Client', 'Client',
+                    ['MySQL' => 'MySQL', 'Elastic' => 'Elastic', 'Algolia' => 'Algolia', 'Swiftype' => 'Swiftype'],
+                    'MySQL'
+                )
             ),
             FieldList::create(
-                FormAction::create('doPropertySearch','Search')->addExtraClass('btn-lg btn-primary')
+                FormAction::create('doPropertySearch', 'Search')->addExtraClass('btn-lg btn-primary')
             )
         );
 
